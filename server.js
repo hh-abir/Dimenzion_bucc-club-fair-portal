69e4ffeb7563e58510d29598cd1f697369f84071
abir <abir4dev@gmail.com>
2025-07-05 20:49:40 +0600

    new modification

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ import { Server } from "socket.io";
 
 
 const dev = process.env.NODE_ENV !== 'production';
-const hostname = 'localhost';
-const port = 3000;
+const hostname = process.env.HOSTNAME || '0.0.0.0';
+const port = process.env.PORT || 3000;
 
 const app = next({ dev, hostname, port });
 const handle = app.getRequestHandler();
